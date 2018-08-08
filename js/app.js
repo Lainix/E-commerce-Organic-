e4aff684f09adcbb0fcb24f119b64f4625133a95
Lainix <linkelin8@gmail.com>
2018-08-07 23:39:13 -0500

    Mejorar de la experiencia de usuario en el html, funcion para segundo parametro de busqueda

--- js/app.js
+++ js/app.js
@@ -1,35 +1,74 @@
 $(document).ready(() => {
-    const mercado = () => {
-        $.ajax({
-            url: `https://api.mercadolibre.com/sites/MLA/search?q=organico`,
-            type: 'GET',
-            crossDomain: true,
-            datatype: 'json',
-            success: function (response) {
-                for (var i = 0; i <= 5; i++) {
-                    var photo = response.results[i].thumbnail;
-                    console.log(photo);
 
-                    var titleProduct = response.results[i].title;
-                    var priceProduct = '$' + '' + response.results[i].price;
-                    var template = `<div class="card" style="width: 18rem;">
+  const mercado = () => {
+    $.ajax({
+      url: `https://api.mercadolibre.com/sites/MLA/search?q=organico`,
+      type: 'GET',
+      crossDomain: true,
+      datatype: 'json',
+      success: function (response) {
+        for (var i = 0; i <= 5; i++) {
+          var photo = response.results[i].thumbnail;
+          console.log(photo);
+
+          var titleProduct = response.results[i].title;
+          var priceProduct = '$' + '' + response.results[i].price;
+          var template = `<div class="card" style="width: 18rem;">
                         <div class="card-body">
-                            <img class="card-img-top" src="${photo}" alt="Card image cap">
+                            <img class="card-img-top" style="width: 10rem;" src="${photo}" alt="Card image cap">
                             <h5 class="card-title">${titleProduct}</h5>
                             <p class="card-text">${priceProduct}</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <a href="#" class="btn btn-primary" style="background-color:#77a8a8">Comprar</a>
                         </div>
                      </div>`;
 
-                    $('#product').append(template);
-                };
-            }
-        }).done((response) => {
-            console.log(response);
-            console.log(response.results["0"].thumbnail);
-        });
-    }
-    mercado();
+          $('#product').append(template);
+        };
+      }
+    }).done((response) => {
+      console.log(response);
+      console.log(response.results["0"].thumbnail);
+    });
+  }
+  mercado();
+
+  const porductosOrganicos = () => {
+    $.ajax({
+      url: `https://api.mercadolibre.com/sites/MLA/search?q=productos%20organico`,
+      type: 'GET',
+      crossDomain: true,
+      datatype: 'json',
+      success: function (response) {
+        for (var i = 0; i <= 5; i++) {
+          var photo = response.results[i].thumbnail;
+          console.log(photo);
+
+          var titleProduct = response.results[i].title;
+          var priceProduct = '$' + '' + response.results[i].price;
+          var template = `<div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <img class="card-img-top" style="width: 10rem;" src="${photo}" alt="Card image cap">
+                            <h5 class="card-title">${titleProduct}</h5>
+                            <p class="card-text">${priceProduct}</p>
+                            <a href="#" class="btn btn-primary" style="background-color:#77a8a8">Comprar</a>
+                        </div>
+                     </div>`;
+
+          $('#productOrganic').append(template);
+        };
+      }
+    }).done((response) => {
+      console.log(response);
+      console.log(response.results["0"].thumbnail);
+    });
+  }
+  porductosOrganicos();
+
+  $('#ejecutar').on('click', function () {
+    $('caja').slideUp(1000, mercado);
+
+  });
+
 });
 
 
