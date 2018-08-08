a80e8587a36cded216a1c8ce8d42939dae7b13be
Lainix <linkelin8@gmail.com>
2018-08-08 15:46:18 -0500

    Mejorar de la experiencia de usuario en el html, funcionalidad a el apartado de productos, desplegable para el carrito de compra

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 $(document).ready(() => {
 
+  $('#ejecutar').on('click', function () {
+    $('.caja').show();
+    $('.caja2').hide();
+  });
+  
+  $('#ejecutar2').on('click', function () {
+    $('.caja2').show();
+    $('.caja').hide();
+  });
+
+  $('#cart').on('click', function () {
+    $('#cart-items').show();
+  });
+
+  $('#close').on('click', function () {
+    $('#cart-items').hide();
+  });
+
   const mercado = () => {
     $.ajax({
       url: `https://api.mercadolibre.com/sites/MLA/search?q=organico`,
@@ -19,6 +37,7 @@ $(document).ready(() => {
                             <h5 class="card-title">${titleProduct}</h5>
                             <p class="card-text">${priceProduct}</p>
                             <a href="#" class="btn btn-primary" style="background-color:#77a8a8">Comprar</a>
+                            <a href="#" class="btn" id="paypal">Paypal</a>
                         </div>
                      </div>`;
 
@@ -50,7 +69,7 @@ $(document).ready(() => {
                             <img class="card-img-top" style="width: 10rem;" src="${photo}" alt="Card image cap">
                             <h5 class="card-title">${titleProduct}</h5>
                             <p class="card-text">${priceProduct}</p>
-                            <a href="#" class="btn btn-primary" style="background-color:#77a8a8">Comprar</a>
+                            <a href="#" class="btn" id="paypal">Paypal</a>
                         </div>
                      </div>`;
 
@@ -64,10 +83,33 @@ $(document).ready(() => {
   }
   porductosOrganicos();
 
-  $('#ejecutar').on('click', function () {
-    $('caja').slideUp(1000, mercado);
-
-  });
+    paypal.Button.render({
+    env: 'sandbox',
+    client: {
+      sandbox: 'demo_sandbox_client_id'
+    },
+    style: {
+      color: 'gold',   // 'gold, 'blue', 'silver', 'black'
+      size:  'medium', // 'medium', 'small', 'large', 'responsive'
+      shape: 'rect'    // 'rect', 'pill'
+    },
+    payment: function (data, actions) {
+      return actions.payment.create({
+        transactions: [{
+          amount: {
+            total: '0.01',
+            currency: 'USD'
+          }
+        }]
+      });
+    },
+    onAuthorize: function (data, actions) {
+      return actions.payment.execute()
+        .then(function () {
+          window.alert('Thank you for your purchase!');
+        });
+    }
+  }, '#paypal-button');
 
 });
 
